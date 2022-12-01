f5a703f551198c3113608dd9dd5fd70c3b4a6c74
Nikita Behmetev <nikitabehmetev@gmail.com>
2022-12-01 13:31:09 +0300

    логика авторизации

--- app/src/main/java/com/grib/messenger/LoginActivity.java
+++ app/src/main/java/com/grib/messenger/LoginActivity.java
@@ -1,13 +1,19 @@
 package com.grib.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,17 +23,25 @@ public class LoginActivity extends AppCompatActivity {
     private TextView textViewForgotPassword;
     private TextView textViewRegister;
 
+    private LoginViewModel loginViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         initViews();
+        loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+        observeViewModel();
+        setupClickListeners();
+    }
+
+    private void setupClickListeners() {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = editTextEmail.getText().toString().trim();
                 String password = editTextPassword.getText().toString().trim();
-                //login
+                loginViewModel.login(email, password);
             }
         });
         textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +58,25 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = RegistrationActivity.newIntent(LoginActivity.this);
                 startActivity(intent);
+            }
+        });
+    }
+
+    private void observeViewModel() {
+        loginViewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                if (s != null) {
+                    Toast.makeText(LoginActivity.this, s, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        loginViewModel.getUser().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser firebaseUser) {
+                if (firebaseUser != null) {
+                    Toast.makeText(LoginActivity.this, "Authorized", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
