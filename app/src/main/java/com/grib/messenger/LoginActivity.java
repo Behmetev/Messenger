53d83265b5cd10bbf2a08a7e69c976a95663d0d2
Nikita Behmetev <nikitabehmetev@gmail.com>
2022-12-01 10:51:10 +0300

    добавил login activity + логику

---
+++ app/src/main/java/com/grib/messenger/LoginActivity.java
@@ -0,0 +1,53 @@
+package com.grib.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewForgotPassword;
+    private TextView textViewRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initViews();
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmail.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                //login
+            }
+        });
+        textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // fp activity
+            }
+        });
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Reg activity
+            }
+        });
+    }
+
+    private void initViews() {
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.login);
+        textViewForgotPassword = findViewById(R.id.editTextPassword);
+        textViewRegister = findViewById(R.id.textViewRegister);
+    }
+}
